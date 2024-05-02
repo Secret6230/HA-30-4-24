3c040d6ed78605a2eda250be8f715583fbc9ba23
Secret6230 <akinkale054@hotmail.com>
2024-05-02 15:50:04 +0200

    HAUSAUFGABEN

---
+++ Übung.js
@@ -0,0 +1,18 @@
+//Aufgabe 1
+let Celsius = prompt();
+
+let Fahrenheit = Celsius * 9/5 + 32
+
+console.log(Fahrenheit)
+
+
+//Aufgabe 2
+let Leere_Liste = [];
+
+for (let i = 0; i < 5; i++){
+    Leere_Liste.push(prompt());
+}
+
+for (Hallo of Leere_Liste){
+    console.log(Hallo)
+}
